63da7c0f6333b691ba46e928b9acc6b36b76d844
Pericles001 <periclesadjovi@gmail.com>
2024-03-14 19:56:47 +0200

    Edit file: Add exception handler for a bad name input

---
+++ python/src/exception/bad_nametype_exception_handler.py
@@ -0,0 +1,31 @@
+"""
+File: bad_nametype_exception_handler.py
+Class: BadNameTypeExceptionHandler - contains the exception handler for the name type exception (when the value is not a string).
+"""
+
+from python.src.exception.exception_handler import ExceptionHandler
+
+
+class BadNameTypeExceptionHandler(ExceptionHandler):
+    """
+    Class: BadNameTypeExceptionHandler
+    Purpose: contains the exception handler for the name type exception (when the value is not a string).
+    - inherits from ExceptionHandler
+    - override the handle_exception method to handle the name type exception
+    """
+
+    @staticmethod
+    def handle_exception(exception, target):
+        """
+        Handle the name type exception.
+
+        Args:
+            exception (Exception): The name type exception to handle.
+            :param exception: the name type exception to handle
+            :param target: the input name value that caused the exception
+        """
+        print(exception)
+        # transform the target value to a string
+        target = str(target)
+        print("The target value has been transformed to a string...")
+        return target
---
+++ python/src/service/event_services.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+File: eventServices.py
+Class: EventServices - contains the services for the event object  (operations on the event object).
+"""
+
+
+class EventServices:
+    
+
