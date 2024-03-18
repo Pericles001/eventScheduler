ef9192cdeb830309e087813b90b088346f16ce49
Pericles001 <periclesadjovi@gmail.com>
2024-03-18 13:15:49 +0200

    Edit file: - Initialize class

---
+++ python/src/client/sys_client.py
@@ -0,0 +1,18 @@
+"""
+File: sys_client.py
+Class: SysClient - contains the client for the system
+
+"""
+
+
+class SysClient:
+    """
+    Class: SysClient
+    Purpose: contains the client for the event scheduler system
+    """
+    def __init__(self):
+        """
+        Method: __init__
+        Purpose: initialize the client
+        """
+        self.client = None
