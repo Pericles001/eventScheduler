3fddea8b4a6f05b2bd030993feca6c5202d4ee23
Pericles001 <periclesadjovi@gmail.com>
2024-03-18 13:16:41 +0200

    Edit file: - Write main function as entry point for application

---
+++ python/main.py
@@ -0,0 +1,26 @@
+"""
+File: main.py
+Class: Main - the main class of the system
+Purpose: Launches the application
+"""
+
+import python.src.ui.event_scheduler_ui as event_scheduler_ui
+
+
+class Main:
+    """
+    Class: Main
+    Purpose: the main class of the system
+    """
+
+    def __init__(self):
+        """
+        Initialize a new main class.
+        """
+        self.ui = event_scheduler_ui.EventSchedulerUI()
+        self.ui.start()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.ui.start()
